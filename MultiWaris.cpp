17a95a0e5a5356834c5faa95e43571125f7a5fc1
LENOVO <MeutyaAzzahrani>
2024-05-27 15:35:43 +0700

    menampilkan class budi

--- MultiWaris.cpp
+++ MultiWaris.cpp
@@ -32,3 +32,14 @@ public:
         }
 };
 
+class budi : public pekerja, public pelajar{
+public:
+      
+      budi(int pUmur) :
+      pekerja(pUmur),
+      pelajar(pUmur)
+      orang(pUmur) // hal ini dapat digunakan jika menggunakan virtual
+      {
+        cout << "Budi dibuat\n" << endl;
+        }
+};
