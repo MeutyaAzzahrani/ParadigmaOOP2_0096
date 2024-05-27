787af8b5bd2d2690a106d2846f1123d74a306a70
LENOVO <MeutyaAzzahrani>
2024-05-27 14:53:25 +0700

    NoOverriding selesai

--- NoOverriding.cpp
+++ NoOverriding.cpp
@@ -3,14 +3,21 @@ using namespace std;
 
 class baseClass {
 public:
-    virtual void perkenalan() final {
+    virtual void perkenalan() {
         cout << "Hallo saya function dari bace class";
     }
 };
 
 class deviredClass : public baseClass {
 public:
-    void perknalan(){
+    void perkenalan(){
         cout << "Hallo saya Function dari derived Class";
     }
+};
+
+int main() {
+    deviredClass a;
+    a.perkenalan();
+
+    return 0;
 };
